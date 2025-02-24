d4e0ed8f2e8e6f62ec58515ce4d94980da2b422f
RitheeshBaradwaj <ritheeshbaradwaj@gmail.com>
2025-02-24 11:31:00 +0900

    chore: add debug flag in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 clean_build=false
+enable_debug=false
 
-while getopts "c" opt; do
+BUILD_FLAGS=""
+
+while getopts "cd" opt; do
   case $opt in
     c)
       clean_build=true
+      ;;
+    d)
+      enable_debug=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -17,8 +23,13 @@ if $clean_build; then
   sudo rm -rf build/
 fi
 
+if $enable_debug; then
+  echo "Enable Debug"
+  BUILD_FLAGS="-DCMAKE_BUILD_TYPE=Debug"
+fi
+
 (mkdir build || true ) && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake ${BUILD_FLAGS} ..
 make
 sudo make install 
 cd ..
